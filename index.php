6255d236b142bf7a368cd446061e34669752d253
yugo <c2068@oic.jp>
2024-10-24 13:58:48 +0900

    メイン画面更新

--- index.php
+++ index.php
@@ -18,15 +18,16 @@
         <!-- ヘッダー-->
         <header>
             <nav id = "header">
-                <h1><img class = "logo" href="index.php" src="image/logo.png" alt="ロゴ"></h1>
-                <h1><img class = "contact" href = "contact.php"src="image/contact.png" alt="お問い合わせ"></h1> 
+                <img class = "logo" href="./index.php" src="image/logo.png" alt="ロゴ">
+                <img class = "hamburger" href = "./contact.php" src="image/hamburger.png" alt="お問い合わせ"> 
+                <h1>New Link</h1>
             </nav>
             
             <nav id = "header_items">
                 <ul>
                     <li><a href="index.php">ホーム</a></li>
                     <li><a href="search.php">お相手を検索</a></li>
-                    <li><a href="like.php">イイね</a></li>
+                    <li><a href="good.php">いいね</a></li>
                     <li><a href="message.php">メッセージ</a></li>
                     <li><a href="news.php">新着情報</a></li>
                     <li><a href="profile.php">プロフィール</a></li>
@@ -38,30 +39,12 @@
         
         <!-- メイン画像-->
         <div>
-            <p><img class="main_img" src="./image/deai.png" alt="メイン画像"></p>
+            <img class="main_img" src="./image/deai.png" alt="メイン画像">
         </div>
         <!--/メイン画像-->
 
 
         <!-- メイン -->
-        <div id="wappaer">
-            <div id="main">
-                <section id="point">
-                    <h2>最近の話題</h2>
-                    <section>
-                        <h3>
-                            出会いの証
-                        </h3>
-                        <figure>
-                            <img class = "akasi_img" src="image/akasi.png" alt="出会いの証">
-                            <figcaption>デートの様子</figcaption>
-                            <p>出会った方々が一緒に遊んでいる様子です。<br>
-                            このアプリで、様々な方が出会っています。</p>
-                        </figure>
-                    </section>
-                </section>
-            </div>
-        </div>
         <!--/メイン -->
 
         <!-- フッター -->
