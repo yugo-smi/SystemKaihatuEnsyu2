20fca839cf358fd56f21ebc7c33c9d0d265c62ec
yugo <c2068@oic.jp>
2024-10-31 17:38:35 +0900

    メイン画面更新

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
         <html lang="ja">
         <title>NEW LINK</title>  
         <link rel="stylesheet" href="css/style_index.css">
-        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <!--/head（ページ概要） -->
@@ -18,38 +17,48 @@
         <!-- ヘッダー-->
         <header>
             <div id = "header">
-                <a href="index.php"><img class = "logo"  src="image/logo.png" alt="ロゴ"></a>
-                <img class = "hamburger_bar" src="image/hamburger.png" alt="お問い合わせ"> 
-                <h1>New Link</h1>
+                <a href="index.php">
+                    <img class = "logo"  src="image/logo.png" alt="ロゴ">
+                </a>
+                    <img class = "hamburger_bar" src="image/hamburger.png" alt="メニュー"> 
+                <div class = "logotitle">
+                    <img src="image/logotitle.png" alt="タイトル">
+                </div>
             </div>
             
-            <nav>
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-            </nav>
-                
             <nav id = "hamburger_items">
                 <ul>
-                    <li><a href="search.php">お相手を検索</a></li>
-                    <li><a href="good.php">いいね</a></li>
-                    <li><a href="message.php">メッセージ</a></li>
-                    <li><a href="news.php">新着情報</a></li>
-                    <li><a href="profile.php">プロフィール</a></li>
+                    <li><a href="search.php">検索画面へ</a></li>
+                    <li><a href="good.php">いいね画面へ</a></li>
+                    <li><a href="message.php">メッセージ画面へ</a></li>
+                    <li><a href="profile.php">プロフィール画面へ</a></li>
                 </ul>
             </nav>
-            
         </header>
         <!--/ヘッダー-->
         
-        <!-- メイン画像-->
+        <!-- メイン-->
+        <h1>
+            New Linkの説明
+        </h1>
+
+        <div>
+            　New Linkは他の学年や分野の学生、留学生など、他クラスの人とも仲良くなれるようというニーズに応えられるよう開発したWEBアプリです。<br>
+            　このアプリはOIC学生専用で開発され、情報漏洩がなく、嫌がらせや荒らしのない快適な環境を目指しています。<br>
+        <div>
+
+        <h1>
+            部活動の紹介👍
+        </h1>
+
         <div class = "main_illust">
             <img src="./image/deai.png" alt="メイン画像">
         </div>
-        <!--/メイン画像-->
 
+        
 
-        <!-- メイン -->
+        
+
         <!--/メイン -->
 
         <!-- フッター -->
