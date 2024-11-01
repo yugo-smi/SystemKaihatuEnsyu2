a7fa717b27fbdfebf45d8d919a587efe25697adc
yugo <c2068@oic.jp>
2024-11-01 18:06:35 +0900

    メイン画面　ハンバーガーバー　追加

---
+++ js/index_hamburger.js
@@ -0,0 +1,6 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    this.classList.toggle("active");
+    document.getElementById("menu").classList.toggle("open");
+  });
+
+
