bcbe5d2f2dbe00ac9e98f1c40591b6a88859d3ee
c2386 <c2386@oic.jp>
2024-11-01 18:01:52 +0900

    20241101

--- js/register.js
+++ js/register.js
@@ -13,4 +13,25 @@ document.addEventListener('DOMContentLoaded', function() {
       confirmPasswordInput.setAttribute('type', type);
       this.querySelector('img').src = type === 'password' ? 'image/eye-icon.png' : 'image/eye-slash-icon.png';
   });
+
+  // フォーム送信時のパスワード確認
+  document.getElementById('registerForm').addEventListener('submit', function(event) {
+      const password = document.getElementById('password').value;
+      const confirmPassword = document.getElementById('confirm-password').value;
+      const pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+      // パスワードが8文字以上で英数字を含むかをチェック
+      if (!pattern.test(password)) {
+          alert("パスワードは英数字を組み合わせた8文字以上で入力してください。");
+          event.preventDefault();
+          return;
+      }
+
+      // パスワードと確認パスワードが一致するかをチェック
+      if (password !== confirmPassword) {
+          alert("パスワードが一致しません。もう一度確認してください。");
+          event.preventDefault(); // フォーム送信をキャンセル
+          return;
+      }
+  });
 });
