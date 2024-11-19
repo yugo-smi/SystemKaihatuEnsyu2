127e381dd9af6926ec977e1562be0b1269f28045
c2386 <c2386@oic.jp>
2024-11-19 17:42:11 +0900

    画像完成

--- profile.php
+++ profile.php
@@ -39,12 +39,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $tags = implode(",", $_POST['tags']);  // 選択されたタグをカンマ区切りに
     $bio = $_POST['bio'];
 
+    //画像投稿の処理
+    $image = $_SESSION['user_id'];//ファイル名を設定
+    $image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);//アップロードされたファイルの拡張子を取得
+    $file = "profile-image/$image";
+    if (!empty($_FILES['image']['name'])) {//ファイルが選択されていれば$imageにファイル名を代入
+        move_uploaded_file($_FILES['image']['tmp_name'], 'profile-image/' . $image);//imagesディレクトリにファイル保存
+        if (exif_imagetype($file)) {//画像ファイルかのチェック
+            $imagepath = $file;
+        } else {
+            $imagepath = null;
+        }
+    }
     // ユーザー情報の更新
-    $update_sql = "UPDATE user_table SET nickname = :nickname, tags = :tags, bio = :bio WHERE id = :id";
+    $update_sql = "UPDATE user_table SET nickname = :nickname, tags = :tags, bio = :bio ,image_path = :image_path WHERE id = :id";
     $update_stmt = $pdo->prepare($update_sql);
     $update_stmt->bindParam(':nickname', $nickname);
     $update_stmt->bindParam(':tags', $tags);
     $update_stmt->bindParam(':bio', $bio);
+    $update_stmt->bindParam(':image_path', $imagepath, PDO::PARAM_STR);
     $update_stmt->bindParam(':id', $user_id, PDO::PARAM_INT);
     $update_stmt->execute();
 
@@ -60,18 +73,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./css/style_partner_profile.css">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>プロフィール編集</title>
 </head>
 <body>
     <h2>プロフィール編集</h2>
-    <form method="POST" action="profile.php">
+    <form method="POST" action="profile.php" enctype="multipart/form-data">
         <!-- 画像選択機能を追加 -->
         <div class="profile-info">
                 <div class="profile-pic-container">
                     <img src="image/default-pic.png" alt="プロフィール画像" id="profile-pic" class="profile-pic">
                     <label for="profile-pic-input" class="file-label">プロフィール画像を選択</label>
-                    <input type="file" id="profile-pic-input" accept="image/*" aria-label="プロフィール画像を選択">
+                    <input type="file" name="image" id="profile-pic-input" aria-label="プロフィール画像を選択">
                 </div>
 
         <label>ニックネーム:</label>
@@ -94,6 +108,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <textarea name="bio"><?= htmlspecialchars($user['bio'], ENT_QUOTES, 'UTF-8') ?></textarea><br>
 
         <button type="submit">更新</button>
+        <?php echo "<p style='color:green;'>プロフィールが更新されました。</p>"; ?>
     </form>
     <script src="js/profile.js"></script>
 </body>
