38eed6043cc329ab0ded962dcd580c54c296b22e
yugo0523 <c2068@oic.jp>
2024-12-03 15:14:15 +0900

    ログアウトボタン追加

--- index.php
+++ index.php
@@ -7,70 +7,65 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
+$isLoggedIn = isset($_SESSION['user_id']);
 ?>
 
-<!-- html -->
 <!DOCTYPE html>
- 
- <!-- head（ページ概要） -->
- <head>
-     <html lang="ja">
-     <title>NEW LINK</title>  
-     <link rel="stylesheet" href="css/style_index.css">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
- </head>
- <!--/head（ページ概要） -->
+<html lang="ja">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEW LINK</title>
+    <link rel="stylesheet" href="css/style_index.css">
+</head>
 
- <!-- body（本文） -->
- <body>
-     <!-- ヘッダー-->
-     <header>
-         <div id = "header">
-             <a href="index.php">
-                 <img class = "logo"  src="image/logo.png" alt="ロゴ">
-             </a>
+<body>
+    <!-- ヘッダー -->
+    <header>
+        <div id="header">
+            <a href="index.php">
+                <img class="logo" src="image/logo.png" alt="ロゴ">
+            </a>
 
-             <div class="hamburger" id="hamburger">
-                 <img src="image/hamburger.png" alt="ハンバーガーバー">
-             </div>
+            <div class="hamburger" id="hamburger">
+                <img src="image/hamburger.png" alt="ハンバーガーメニュー">
+            </div>
 
-             <!-- メニュー -->
-             <nav class="menu" id="menu">
-                 <ul>
-                     <li><a href="index.php">ホーム</a></li>
-                     <li><a href="kensaku.php">お相手を検索</a></li>
-                     <li><a href="message.php">スレッド</a></li>
-                     <li><a href="chat.php">メッセージ</a></li>
-                     <li><a href="profile.php">プロフィール</a></li>
-                 </ul>
-             </nav>
+            <!-- メニュー -->
+            <nav class="menu" id="menu">
+                <ul>
+                    <li><a href="index.php">ホーム</a></li>
+                    <li><a href="kensaku.php">お相手を検索</a></li>
+                    <li><a href="message.php">スレッド</a></li>
+                    <li><a href="chat.php">メッセージ</a></li>
+                    <?php if ($isLoggedIn): ?>
+                        <li><a href="profile.php">プロフィール</a></li>
+                        <li><a href="logout.php">ログアウト</a></li>
+                    <?php else: ?>
+                        <li class="logout"><a href="login.php">Logout</a></li>
+                    <?php endif; ?>
+                </ul>
+            </nav>
 
-             <div class = "logotitle">
-                 <img src="image/logotitle.png" alt="タイトル">
-             </div>
-         </div>
-     </header>
-     
-     <!--/ヘッダー-->
-     
-     <!-- メイン-->
+            <div class="logotitle">
+                <img src="image/logotitle.png" alt="タイトル">
+            </div>
+        </div>
+    </header>
+    <!--/ヘッダー-->
 
-
-     <!-- <div class = "main_illust">
-         <img src="./image/deai.png" alt="メイン画像">
-     </div> -->
-
+    <!-- メイン -->
     <div class="main-container">
         <div class="slideshow-container">
             <div class="slide">
-            <img src="./image/deai.png" alt="Slide 1">
+                <img src="./image/deai.png" alt="Slide 1">
             </div>
             <div class="slide">
-            <img src="./image/akasi.png" alt="Slide 2">
+                <img src="./image/akasi.png" alt="Slide 2">
             </div>
             <div class="slide">
-            <img src="./image/default-pic.png" alt="Slide 3">
+                <img src="./image/default-pic.png" alt="Slide 3">
             </div>
         </div>
         <div class="dots-container">
@@ -79,41 +74,21 @@ if (!isset($_SESSION['user_id'])) {
             <span class="dot" onclick="currentSlide(3)"></span>
         </div>
     </div>
-          <!-- インジケーターのドットを追加 -->
 
- <script src="js/index_hamburger.js"></script>
- <!-- ランダムマッチングボタン -->
- <a href="./partner_profile.php" class="random_matching_button">ランダムマッチングボタン</a>
+    <!-- ランダムマッチングボタン -->
+    <a href="./partner_profile.php" class="random_matching_button">ランダムマッチングボタン</a>
 
+    <!-- スクリプト -->
+    <script src="js/index_hamburger.js"></script>
+    <script src="./js/index_slideshow.js"></script>
+    <!--/メイン -->
 
-     <script src="./js/index_slideshow.js"></script>   
-     <!--/メイン -->
+    <!-- フッター -->
+    <footer>
+        <!-- フッター内容 -->
+    </footer>
+    <!--/フッター -->
 
-     <!-- フッター -->
-     <footer>
-        
-     </footer>
-     <!--/フッター -->
-
- </body>
- <!--/body（本文） -->
+</body>
 
 </html>
-<!--/html -->
-
-<?php
-session_start();
-$isLoggedIn = isset($_SESSION['user_id']);
-?>
-<nav class="menu" id="menu">
- <ul>
-     <li><a href="index.php">ホーム</a></li>
-     <?php if ($isLoggedIn): ?>
-         <li><a href="profile.php">プロフィール</a></li>
-     <?php else: ?>
-         <li><a href="login.php">ログインが必要です</a></li>
-     <?php endif; ?>
-     <li><a href="">PayPay</a></li>
-     <li><a href="">QuickPay</a></li>
- </ul>
-</nav>
